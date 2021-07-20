ffe3405e94fa2bc9e664166ddb7025d023b6401d
xaaphrodite <79451877+xaaphrodite@users.noreply.github.com>
2021-07-20 19:02:49 +0700

    improved

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 const api = document.getElementById("api");
-api.innerHTML = `${window.location.protocol}${window.location.hostname}:${window.location.port}/api/mevn`;
+api.innerHTML = `${window.location.protocol}${window.location.hostname}:${window.location.port}/api/node`;
 
 const author = document.getElementById("author");
 author.innerHTML = "by rasetiansyah";
